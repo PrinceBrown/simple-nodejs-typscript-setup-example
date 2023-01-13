b6572f16033438ae612295b3922c4017130291b7
Prince Brown <codingproprince@gmail.com>
2023-01-13 16:56:34 -0500

    Example: setup  nodejs server with typescript

---
+++ app.ts
@@ -0,0 +1,14 @@
+import express, { Request, Response } from 'express';
+
+const app = express();
+
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => { console.log(`Server is listening on PORT ${PORT}`);});
